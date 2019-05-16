feec461dc5d9b3cf65dedea93bf055e60685b823
Nachman Savin <nachmansavin@gmail.com>
2019-05-16 12:52:23 +0300

    Added Cat Names above images.

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,18 @@ var elem = document.getElementById('cat-pic1');
 var elem2 = document.getElementById('cat-pic2');
 var clicks1 = 0;
 var clicks2 = 0;
+var catname1 = 'Pickels';
+var catname2 = 'Paws';
+
+$('#cat-name1').html(catname1);
+$('#cat-name2').html(catname2);
+
 elem.addEventListener('click', function(){
   clicks1 ++;
-  $("#counter1").html("You clicked on the cat " + clicks1 + " times.");
+  $("#counter1").html("You clicked on " + catname1 + " " + clicks1 + " times.");
 }, false);
 
 elem2.addEventListener('click', function() {
   clicks2 ++;
-  $("#counter2").html("You clicked on the cat " + clicks2 + " times.");
+  $("#counter2").html("You clicked on " + catname2 + " " + clicks2 + " times.");
 }, false);
