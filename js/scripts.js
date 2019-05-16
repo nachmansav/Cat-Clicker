89cfc1580f8f37bb02c512f435e7b2c8df9d5c29
Nachman Savin <nachmansavin@gmail.com>
2019-05-16 12:43:57 +0300

    added second cat with second counter

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
-var elem = document.getElementById('cat-pic');
-var clicks = 0
+var elem = document.getElementById('cat-pic1');
+var elem2 = document.getElementById('cat-pic2');
+var clicks1 = 0;
+var clicks2 = 0;
 elem.addEventListener('click', function(){
-  clicks ++;
-  $("#counter").html("You clicked on the cat " + clicks + " times.")
+  clicks1 ++;
+  $("#counter1").html("You clicked on the cat " + clicks1 + " times.");
 }, false);
 
+elem2.addEventListener('click', function() {
+  clicks2 ++;
+  $("#counter2").html("You clicked on the cat " + clicks2 + " times.");
+}, false);
