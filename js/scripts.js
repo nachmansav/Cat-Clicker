6dfb9c5e0ba3c1248daa7d909e8908cc84ca3198
Nachman Savin <nachmansavin@gmail.com>
2019-05-19 11:41:44 +0300

    added 5 cats to click on

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,27 @@
-var elem = document.getElementById('cat-pic1');
-var elem2 = document.getElementById('cat-pic2');
-var clicks1 = 0;
-var clicks2 = 0;
-var catname1 = 'Pickels';
-var catname2 = 'Paws';
+var catnames = ['Pickels', 'Paws', 'Salt & Pepper', 'Dominoe', 'Cardboard'];
+var srcs = ["https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496",
+"https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426",
+"https://lh5.ggpht.com/LfjkdmOKkGLvCt-VuRlWGjAjXqTBrPjRsokTNKBtCh8IFPRetGaXIpTQGE2e7ZCUaG2azKNkz38KkbM_emA=s0#w=640&h=454",
+"https://kittentoob.com/wp-content/uploads/2018/11/Cat-Toys.jpg",
+"https://boygeniusreport.files.wordpress.com/2015/06/funny-cat.jpg?quality=98&strip=all&w=782"];
 
-$('#cat-name1').html(catname1);
-$('#cat-name2').html(catname2);
+for (var i = 0; i < catnames.length; i++) {
+	$('#catList').append('<li>'+ catnames[i] + '</li>');
 
-elem.addEventListener('click', function(){
-  clicks1 ++;
-  $("#counter1").html("You clicked on " + catname1 + " " + clicks1 + " times.");
-}, false);
+	$('#cat-name').html(catnames[i]);
+	
+	$('#cat-pic').html('<img src=' + srcs[i] + '>');
+}
 
-elem2.addEventListener('click', function() {
-  clicks2 ++;
-  $("#counter2").html("You clicked on " + catname2 + " " + clicks2 + " times.");
-}, false);
+
+
+
+
+  
+  //$("#counter1").html("You clicked on " + catname1 + " " + clicks1 + " times.");
+//}, false);
+
+//elem2.addEventListener('click', function() {
+  //clicks2 ++;
+  //$("#counter2").html("You clicked on " + catname2 + " " + clicks2 + " times.");
+//}, false);
