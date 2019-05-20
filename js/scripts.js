d57d758387f8c2b0874069692244a132f11f6b43
Nachman Savin <nachmansavin@gmail.com>
2019-05-20 10:06:39 +0300

    added new method of loading cats that is scalable

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,27 @@ var srcs = ["https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeY
 "https://lh5.ggpht.com/LfjkdmOKkGLvCt-VuRlWGjAjXqTBrPjRsokTNKBtCh8IFPRetGaXIpTQGE2e7ZCUaG2azKNkz38KkbM_emA=s0#w=640&h=454",
 "https://kittentoob.com/wp-content/uploads/2018/11/Cat-Toys.jpg",
 "https://boygeniusreport.files.wordpress.com/2015/06/funny-cat.jpg?quality=98&strip=all&w=782"];
+var counters = [0,0,0,0,0];
+var attached = [false,false,false,false,false]
 
 for (var i = 0; i < catnames.length; i++) {
-	$('#catList').append('<li>'+ catnames[i] + '</li>');
+	$('#catList').append('<li id=cat' + i + '>'+ catnames[i] + '</li>');
 
-	$('#cat-name').html(catnames[i]);
-	
-	$('#cat-pic').html('<img src=' + srcs[i] + '>');
-}
+	document.getElementById("cat"+ i).addEventListener('click', (function(icopy){
+		return function(){
+		$('#catName').html(catnames[icopy]);
+		$('#catPic').html('<img src= '+ srcs[icopy] + '>');
+		$('#counter').html('');
+		if (attached[icopy] == false){
+		 document.getElementById("catPic").addEventListener('click', function(){
+			counters[icopy] ++;
+			attached[icopy] = true;
+			$('#counter').html("You clicked on " + catnames[icopy] + " " + counters[icopy] + " times.");
+
+		});
+		};
+	};
+		})(i));
 
 
-
-
-
-  
-  //$("#counter1").html("You clicked on " + catname1 + " " + clicks1 + " times.");
-//}, false);
-
-//elem2.addEventListener('click', function() {
-  //clicks2 ++;
-  //$("#counter2").html("You clicked on " + catname2 + " " + clicks2 + " times.");
-//}, false);
+};
